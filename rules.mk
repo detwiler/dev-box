cf3518f0059a1c00ccba01d05f03c04bd186203c
Mike Detwiler <mike@detwiler.io>
2021-01-02 14:46:06 -0500

    Issue #1: Further refactoring
    Signed-off-by: Mike Detwiler <mike@detwiler.io>

--- rules.mk
+++
@@ -1,7 +0,0 @@
-# rules.mk
-
-# generate an output file from an input template via sed
-# EDIT should be a locally defined sed expression.
-# e.g.: EDIT = -e 's:@pkgdatadir[@]:$(pkgdatadir):g'
-%: %.in
-	$(AM_V_GEN)$(SED) $(EDIT) < $< > $@
